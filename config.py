b04978f6ffec2c678f19b658fb16a8cf4b013063
Jathavaan Shankarr <jathavaan12@gmail.com>
2023-02-12 18:10:08 +0100

    refactor: Added track path notebook

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from enum import Enum
 
 class Config(Enum):
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # Path to the root directory of the project
-
+    IMG_DIR = os.path.join(ROOT_DIR, 'report', 'images')  # Path to the images directory
