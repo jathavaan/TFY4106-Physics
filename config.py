d88816a922bb3109ed73c9d56b621a108d2bbf66
Jathavaan Shankarr <jathavaan12@gmail.com>
2023-03-02 14:04:13 +0100

    refactor: Added data from lab 3

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ from enum import Enum
 
 class Config(Enum):
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # Path to the root directory of the project
+    DATA_DIR = os.path.join(ROOT_DIR, 'src', 'data')  # Path to the data directory
     IMG_DIR = os.path.join(ROOT_DIR, 'report', 'images')  # Path to the images directory
